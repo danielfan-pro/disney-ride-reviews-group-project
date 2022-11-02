be1cc59d11b591c1c8a3718e54785cdfa1e6b8fd
Daniel Fan <xuhuafan@gmail.com>
2022-11-02 17:02:15 -0400

    Show Page fix and add username for user

--- app/controllers/api/v1/rides_controller.rb
+++ app/controllers/api/v1/rides_controller.rb
@@ -5,11 +5,12 @@ class Api::V1::RidesController < ApplicationController
     render json: Ride.all
   end
 
-  def show
-    
+  def show   
     render json: Ride.find(params[:id]), serializer: RideShowSerializer, include: ['reviews.user']
+  end
 
   def search
     render json: Ride.where("name ILIKE ?", "%#{params['search_string']}%")
   end
+  
 end
