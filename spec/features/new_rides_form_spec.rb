197d8e41c97e37755ff540c354d232af13702198
Ziming Chen <zimingchen153@gmail.com>
2022-11-02 11:39:02 -0400

    made PR changes

--- spec/features/new_rides_form_spec.rb
+++ spec/features/new_rides_form_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
-feature 'Admin adds new ride', %Q{
-  As an admin
-  I want to create a new ride
-  So that I can add a new ride to the Index page
-} do
+feature 'Admin adds new ride' do
   scenario 'Visit the new ride form page' do
 
     visit new_ride_path
@@ -16,7 +12,7 @@ feature 'Admin adds new ride', %Q{
     visit new_ride_path
 
     fill_in 'Ride Name', with: 'Example Ride Name'
-    select "Adventureland", :from => 'Ride Location'
+    select "Adventureland", from: 'Ride Location'
     fill_in 'Image url', with: 'https://i.gyazo.com/8440cdb48b31c51464c9b403009f4545.png'
     fill_in 'Ride Description', with: 'Example Ride Description'
     click_button 'Create Ride'
@@ -29,7 +25,7 @@ feature 'Admin adds new ride', %Q{
     visit new_ride_path
 
     fill_in 'Ride Name', with: 'Example Ride Name'
-    select "Adventureland", :from => 'Ride Location'
+    select "Adventureland", from: 'Ride Location'
     fill_in 'Image url', with: 'https://i.gyazo.com/8440cdb48b31c51464c9b403009f4545.png'
     click_button 'Create Ride'
 
